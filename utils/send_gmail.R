14704cb9e0c1b1fb96982beb48d587260cf4acad
Watson,Sydeaka <sydeakawatson@gmail.com>
2018-11-04 20:15:56 -0600

    Analysis of 2016 Q2 Lending Club dataset

--- utils/send_gmail.R
+++ utils/send_gmail.R
@@ -6,22 +6,22 @@ library(getPass)
 
 
 
-send_gmail = function(cred_file='~/gmail.txt', email_body, attached_files=NULL,
+send_gmail <- function(cred_file='~/gmail.txt', email_body, attached_files=NULL,
 	Gmail_address_from, email_address_to, email_subject) {
 
 	## Read in credentials
 	msg('Read in credentials')
 	if (!file.exists(cred_file)) {
-	    password_message = paste0('Credentials file not found. Enter Gmail password for ', Gmail_address_from, ':')
-	    gmail_password=getPass(password_message)
+	    password_message <- paste0('Credentials file not found. Enter Gmail password for ', Gmail_address_from, ':')
+	    gmail_password <- getPass(password_message)
 	  } else {
-	    gmail_password = readLines(cred_file)
+	    gmail_password <- readLines(cred_file)
 	}
 
 
 	## Send the email
 	msg('Send the email')
-	email_function_failwith = plyr::failwith(NULL, function(...) send.mail(from = Gmail_address_from,
+	email_function_failwith <- plyr::failwith(NULL, function(...) send.mail(from = Gmail_address_from,
 	    to = email_address_to,
 	    subject = email_subject,
 	    body = email_body,
@@ -33,7 +33,7 @@ send_gmail = function(cred_file='~/gmail.txt', email_body, attached_files=NULL,
 	    attach.files = attached_files
 	    ), quiet=F)
 
-	result = email_function_failwith()
+	result <- email_function_failwith()
 	if (is.null(result)) {
 		stop('To use mailR utilities with a gmail account, you must allow less secure apps.
 		 Please visit https://myaccount.google.com/lesssecureapps and turn this feature ON.')
